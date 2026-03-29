28589ac219728e4ecb65cec70de118b8cf375a94
ernandes-sobreira <156730719+ernandes-sobreira@users.noreply.github.com>
2026-03-29 07:55:10 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/fishrep/",   // <- coloque o nome EXATO do seu repositório GitHub aqui
-});
